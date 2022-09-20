4afc891e9b9c45cdfda479133669cb4244882cb1
tangyu <1833428194@qq.com>
2022-09-20 13:56:50 +0800

    Update

---
+++ DesignPatterns/结构型模式/ProxyPattern.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DesignPatterns_ProxyPattern
+{
+    class ProxyPattern
+    {
+        public void Run()
+        {
+            Proxy proxy = new Proxy();
+            proxy.Func();
+        }
+    }
+
+    interface IFunc
+    {
+        void Func();
+    }
+
+    public class Proxy : IFunc
+    {
+        private Real real;
+
+        public Proxy()
+        {
+            real = new Real();
+        }
+
+        public void Func()
+        {
+            real.Func();
+        }
+    }
+
+    public class Real : IFunc
+    {
+        public void Func()
+        {
+        }
+    }
+}
